30a6fb4ea026e0378044e4586e2857e0d7ccb09f
shobhitjawa <39236815+shobhitjawa@users.noreply.github.com>
2019-09-21 15:05:34 +0530

    Add files via upload

---
+++ arraylist1.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+public class ArrayList1{  
+ public static void main(String args[]){  
+ArrayList<String> list=new ArrayList<String>();    
+      list.add("Audi");    
+      list.add("BMW");    
+      list.add("Merc");    
+      list.add("Lexus");    
+      System.out.println(list);  
+  }  
+ }  
+ 
