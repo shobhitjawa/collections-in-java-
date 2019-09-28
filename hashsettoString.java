0ed45da4265dcf5eadcafc1d948f30edc9c3bf18
shobhitjawa <39236815+shobhitjawa@users.noreply.github.com>
2019-09-28 13:45:29 +0530

    Add files via upload

---
+++ hashsettoString.java
@@ -0,0 +1,12 @@
+ import java.util.*; 
+ public class xyz{
+    public static void main(String[] args) { 
+      HashSet<String> h1=new HashSet<String>();
+      h1.add("shobhit");
+      h1.add("jawa");
+      h1.add("jawa");
+     // System.out.print(h1);     //hashset to String in java collection.
+      String s=h1.toString();
+      System.out.print(s);
+    } 
+} 
